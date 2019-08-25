6913fdea2ebf1e8e2b716f166dbc05fd526b7985
MtrosK5005 <hugo-cobos>
2019-08-24 19:39:23 -0600

    first commit

---
+++ service.js
@@ -0,0 +1,12 @@
+const jwt = require('jwt-simple');  
+const moment = require('moment');  
+const config = require('./config');
+
+exports.createToken = function(user) {  
+  let   payload = {
+        sub: user._id,
+        iat: moment().unix(),
+        exp: moment().add(14, "days").unix(),
+  }
+  return jwt.encode(payload, config.TOKEN_SECRET);
+}
